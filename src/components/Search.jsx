006b9b595e01d363c90081f1043690d50899927e
Jayshree Morey <120532991+jayashreemore@users.noreply.github.com>
2024-04-25 20:29:12 -0400

    getting CORS POLICY ERROR asking to create backened so using JSONP or by utilizing a proxy server that allows CORS requests , ll try and change the code n if its working ll continue

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,51 @@
+import { useState } from "react";
+import md5 from "md5";
+
 
 export default function Search() {
-    const handleSubmit = () => {
-        // ll use later 
+    const [characterName, setCharacterName] = useState("");
+    const [characterData, setCharacterData] = useState("null");
+    const [comicData, setComicData] = useState("null");
+
+    const PUBLIC_API_KEY = import.meta.env.VITE_PUBLIC_API_KEY;
+    const PRIVATE_API_KEY= import.meta.env.VITE_PRIVATE_API_KEY;
+
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        getCharacterData();
     };
 
-    const handleChange = () => {
-        // ll use later 
+    const generateHash = (timeStamp) => {
+        return md5(timeStamp  + PUBLIC_API_KEY + PRIVATE_API_KEY);
+
+    };
+
+    const getCharacterData = () => {
+        setCharacterData(null);
+        setComicData(null);
+
+        const timeStamp = new Date().getTime();
+        const hash = generateHash(timeStamp);
+
+        //const url = `https://gateway.marvel./v1/public/characters?apikey=${publicKey}&hash=${hash}&ts=${timeStamp}&nameStartsWith=${characterName}&limit=100`;
+        const url = `https://gateway.marvel.com/v1/public/characters/?apikey=${PUBLIC_API_KEY}&ts=${timeStamp}&hash=${hash}`;
+        fetch(url)
+            .then((response) => response.json())
+            .then((result) => {
+                setCharacterData(result.data);
+                console.log(result.data);
+            })
+
+            .catch(() => {
+                console.log('error while getting Character Data');
+
+            });
+        
+    }
+
+    const handleChange = (event) => {
+        setCharacterName(event.target.value);
     };
 
     const handleReset = () => {
@@ -15,19 +55,20 @@ export default function Search() {
     return (
         <>
             <form className="search" onSubmit={handleSubmit}>
-    <input
-                type="text"
-                placeholder="Enter Character Name"
-                onChange={handleChange}
-            />
-            <div className="buttons">
-                <button type="submit">Get Chracter Data</button>
-                <button type="reset" className="reset" onClick={handleChange}>
-                    Reset
-                </button>
-            </div>
-        </form>
-    </>
-  );
+                <input
+                    type="text"
+                    placeholder="Enter Character Name"
+                    onChange={handleChange}
+                />
+                <div className="buttons">
+                    <button type="submit">Get Chracter Data</button>
+                    <button type="reset" className="reset" onClick={handleChange}>
+                        Reset
+                    </button>
+                </div>
+            </form>
+        </>
+    );
 }
-
+// buttons to search characters name and reset it
+// 
